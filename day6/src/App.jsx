edd195f213b859978073f73de68785006ac21453
H4xN-Harsh <harshxvillen74@gmail.com>
2026-01-01 12:40:24 +0530

    motion p2

--- day6/src/App.jsx
+++ day6/src/App.jsx
@@ -1,29 +1,39 @@
 import { motion } from "motion/react";
 function App(){
   return(
-    <div>
-      <motion.div
-      initial={{
-        x:0,
-      }}
-       animate={{
+    // <div>
+    //   <motion.div
+    //   initial={{
+    //     x:0,
+    //   }}
+    //    animate={{
 
-        x:300,
-        rotate:360,
+    //     x:300,
+    //     rotate:360,
         
         
-      }}
+    //   }}
 
-      transition={{
-        duration:2,
-        repeat:3,
-        ease:'anticipate'
-      }}
-      className="box">
+    //   transition={{
+    //     duration:2,
+    //     repeat:3,
+    //     ease:'anticipate'
+    //   }}
+    //   className="box">
 
-      </motion.div>
+    //   </motion.div>
       
-    </div>
+    // </div>
+    <motion.div className="box"
+    animate={{
+      x:[0,800,800,0,0],
+      y:[0,0,300,300,0],
+    }}
+    transition={{
+      duration:3,
+      delay:1,
+    }}
+    ></motion.div>
   )
 }
 export default App; 
